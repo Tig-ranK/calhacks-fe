967fc2b50697e56fe7810e9c5da6819428cd1e35
Tigran Karakhanyan <karakhanyant@gmail.com>
2023-10-28 17:46:47 -0700

    Config Prettier and Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import '@mantine/core/styles.css'
+import { MantineProvider, RingProgress, Text } from '@mantine/core'
+import { theme } from './theme'
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+   return (
+      <MantineProvider theme={theme}>
+         <RingProgress
+            label={
+               <Text size="xs" ta="center">
+                  Application data usage
+               </Text>
+            }
+            sections={[{ value: 70, color: 'green', tooltip: '70%' }]}
+         />
+      </MantineProvider>
+   )
 }
