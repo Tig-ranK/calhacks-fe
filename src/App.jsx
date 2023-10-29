4fb66f7f8ebac1380212ffc2231d497b0072598a
Tigran Karakhanyan <karakhanyant@gmail.com>
2023-10-29 03:39:33 -0700

    A bunch of updates + populated more data + fix rings and line charts + add right column with sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import { Flex, RingProgress, Space, Title } from "@mantine/core";
+import {
+   Center,
+   Flex,
+   Grid,
+   RingProgress,
+   Space,
+   Text,
+   Title,
+} from "@mantine/core";
 import "./App.css";
 import {
    LineChart,
@@ -7,46 +15,151 @@ import {
    XAxis,
    YAxis,
    Tooltip,
+   Legend,
 } from "recharts";
 import { emotions } from "./assets/emotions";
 
 export const Ring = ({ emotion, intensity, color = "#101010" }) => (
-   <RingProgress label={emotion} sections={[{ value: intensity, color }]} />
+   <RingProgress
+      roundCaps
+      label={
+         <Text size="xs" ta="center">
+            {emotion}
+         </Text>
+      }
+      sections={[
+         { value: intensity, color, tooltip: `${Math.round(intensity)}%` },
+      ]}
+   />
 );
+
+const colorOf = {
+   joy: "#FFEB3B",
+   anger: "#F44336",
+   sadness: "#2196F3",
+   fear: "#FF5722",
+   surprise: "#4CAF50",
+   disgust: "#9C27B0",
+   trust: "#00BCD4",
+   anticipation: "#FF9800",
+};
+
+function getLastWeekEntries(data) {
+   const today = new Date();
+   const lastWeekStartDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - 7
+   );
+   const lastWeekEndDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - 1
+   );
+
+   const formattedStartDate = lastWeekStartDate.toISOString().split("T")[0];
+   const formattedEndDate = lastWeekEndDate.toISOString().split("T")[0];
+
+   return data.filter(
+      (entry) =>
+         entry.date >= formattedStartDate && entry.date <= formattedEndDate
+   );
+}
+
+// There should be only one entry per day in the data
+// As daily reports are cumulative
+
+const lastWeekEmotions = emotions.slice(-7);
+const lastDayEmotions = emotions.at(-1);
+
+function calculateAverage(data) {
+   const averages = {};
+
+   data.forEach((entry) => {
+      for (const emotion in entry) {
+         if (emotion !== "date") {
+            if (!averages[emotion]) {
+               averages[emotion] = 0;
+            }
+
+            averages[emotion] += entry[emotion];
+         }
+      }
+   });
+
+   // Calculate the average for each emotion
+   for (const emotion in averages) {
+      averages[emotion] /= data.length;
+   }
+
+   return averages;
+}
+
+// const weeklyAverages = calculateAverage();
 
 const renderLineChart = (
    <LineChart
       width={600}
       height={300}
-      data={emotions}
+      data={lastWeekEmotions}
       margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
    >
-      <Line type="monotone" dataKey="joy" stroke="#8884d8" />
-      <Line type="monotone" dataKey="anger" stroke="#8884d8" />
+      {Object.keys(colorOf).map((emotion) => (
+         <Line
+            key={emotion}
+            type="monotone"
+            dataKey={emotion}
+            stroke={colorOf[emotion]}
+         />
+      ))}
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="name" name="Date" />
-      <YAxis name="Intensity" dataKey={date} />
+      <YAxis name="Intensity" />
       <Tooltip />
+      <Legend />
    </LineChart>
 );
 
 function App() {
    return (
       <>
-         {/* <Today />  */}
-         <Title>Today</Title>
-         <Space h="xl" />
-         <Flex>
-            <Ring emotion="Admiration" intensity={40} />
-            <Ring emotion="Admiration" intensity={40} />
-            <Ring emotion="Admiration" intensity={40} />
-            <Ring emotion="Admiration" intensity={40} />
-            <Ring emotion="Admiration" intensity={40} />
-         </Flex>
-         <Title>Last Week</Title>
-         <Space h="xl" />
-         {renderLineChart}
-         {/* <LastWeek /> */}
+         <Grid>
+            <Grid.Col span={6}>
+               <Title>Today</Title>
+               <Space h="xl" />
+               <Flex wrap={"wrap"}>
+                  {Object.keys(colorOf).map((emotion) => (
+                     <Ring
+                        key={emotion}
+                        emotion={emotion}
+                        intensity={lastDayEmotions[emotion] * 100}
+                        color={colorOf[emotion]}
+                     />
+                  ))}
+               </Flex>
+               <Space h="xl" />
+               <Title>Last Week</Title>
+               <Space h="xl" />
+               {renderLineChart}
+            </Grid.Col>
+            <Grid.Col span={6}>
+               <Title>Highlight</Title>
+               <Space h="xl" />
+               <Text>
+                  Given the notably high level of surprise this time, can you
+                  think back to what might have sparked that surprising 0.69
+                  feeling?
+               </Text>
+               <Space h="xl" />
+               <Title>Action</Title>
+               <Space h="xl" />
+               <Text>
+                  Consider exploring more of these unexpected experiences to add
+                  an element of excitement to your routine. Embrace surprises as
+                  opportunities for growth and new perspectives.
+               </Text>
+            </Grid.Col>
+         </Grid>
       </>
    );
 }
